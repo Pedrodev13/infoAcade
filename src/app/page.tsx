1428a3fe8b32be1ee3fbb99bf12f61d215ea966d
PedroDev <pedrodev0202@gmail.com>
2024-06-28 11:31:47 -0300

    ggg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Hero } from '../Components/Hero'
 import "tailwindcss"
 
 const Page = () => (
-  <div className="mt-8 mx-10 h-full md:mx-20">
+  <div className="">
     <Hero />
   </div>
 )
