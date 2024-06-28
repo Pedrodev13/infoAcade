68a6bed059e96a100e758ae30d96c187b237b0d0
PedroDev <pedrodev0202@gmail.com>
2024-06-28 10:52:00 -0300

    consertando a build

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -8,11 +8,11 @@ export const Hero = () => (
             <img className="" src={Imagem.src} alt="Logo" />
         </div>
             <div className="my-10">
-                <div className="gap-28 flex md:justify-center">
+                <div className="gap-28 flex md:justify-center md:items-center">
                     <div className="md:h-auto">
-                    <div className="text-white font-montserrat text-4xl lg:text-[90px] lg:leading-none">Inicie do zero <br /> <span className="text-[#acea00]">informática</span> e <br /> <span className="text-[#acea00]">programação</span>. <br />
+                    <div className="text-white font-montserrat text-4xl lg:text-[75px] lg:leading-none lg:mr-12">Inicie do zero <br /> <span className="text-[#acea00]">informática</span> e <br /> <span className="text-[#acea00]">programação</span>. <br />
                     Especial para <br /> Cardeal da Silva.</div>
-                    <p className="text-white text-[12px]">Informática e programação do <br /> zero no conforto da sua casa.</p>
+                    <p className="text-white text-[12px] lg:text-[20px] font-montSerrat-regular">Informática e programação do <br /> zero no conforto da sua casa.</p>
                     </div>
                     <div className="mt-10 md:mt-0">
                     <img src={Central.src} alt="" />
